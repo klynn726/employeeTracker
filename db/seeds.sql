4af8273daff09eab42b1492b753e087a1ca44345
klynn726 <49536087+klynn726@users.noreply.github.com>
2021-04-28 20:30:14 -0700

    basic file setup schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,48 @@
+INSERT INTO employee (first_name, last_name)
+VALUES
+(Francis, Drake),
+(Walter, Raleigh),
+(Mary, Stewart),
+(Christopher, Marlow),
+(Robert, Dudley),
+(Elizabeth, Tudor),
+(Pope Pius, V),
+(Mary, Tudor),
+(Robert, Cecil),
+(William, Shakespear),
+(Francis, Bacon),
+(Francis, Walsingham),
+(Philip, Sydney),
+(Nicholas, Hilliard),
+(Richard, Burbage),
+(Richard, Grenville),
+(Robert, Johnson),
+(Richard, Hooker),
+(Black Sam, Bellamy),
+(William, Kidd),
+(Henry, Morgan),
+(Calico, Jack),
+(Black, Bart),
+(Blackbeard, Teach),
+(Charles, Vane),
+(Anne, Bonny),
+(Mary, Reed),
+(Cleopatra, Philopator),
+(Catherine, Ulrich)
+
+INSERT INTO department (name)
+VALUES
+(Engineering), 
+(Operations)
+
+
+INSERT INTO role (title, salary)
+VALUES
+(Operation Specialist, 30000),
+(Electronics Technician, 30000),
+(Boatswains Mate, 300000),
+(Gunners Mate, 300000),
+(Machinery Technician, 300000),
+(Damage Controlman, 300000)
+
+
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const db = require('./db/database');
+
+const PORT = process.env.PORT || 3005;
+const app = express();
+
+const apiRoutes = require('./routes/apiRoutes');
+app.use('/api', apiRoutes);
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Default response for any other request(Not Found) Catch all
+app.use((req, res) => {
+  res.status(404).end();
+}); 
+
+// Start server after DB connection
+db.on('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
